d9f7ef366358cb5b6345c0a494f9a8ea81e066b1
Hari Krishna Majety <majetyhk@gmail.com>
2015-02-09 16:58:27 +0530

    PersonalInfo validation on save
    Completed validate on save function for personal info.

--- helperFunctions.php
+++ helperFunctions.php
@@ -61,6 +61,18 @@ function hasOnlyCharacters($characters,$string)
 }
 
 
+function followsRegex($regex,$string)
+{
+	if(preg_match($regex, $string))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 function validateDate($rawDate,$seperator='/')
 	{
 		$ndate=explode($seperator,$rawDate);
@@ -112,61 +124,180 @@ function validateDate($rawDate,$seperator='/')
 //if($_POST["Full_Name"]!="" || $_POST["gender"]!="" || $_POST["date1"] !=""  || $_POST["fname"]!="" || $_POST["Nationality"]!="" || $_POST["Marital_status"]!="" || $_POST["Physically_challenged"]!="" || $_POST["community"]!="" || $_POST["pemail"]!="" || $_POST["aemail"]!="" || $_POST["Temp_Address"]!="" || $_POST["T_District"]!="" || $_POST["T_state"]!="" || $_POST["T_pincode"]!="" || $_POST["T_phone_number"]!="" ||$_POST["T_mobile_number"]!="" || $_POST["perm_Address"]!="" || $_POST["P_District"]!="" || $_POST["P_state"]!="" || $_POST["P_pincode"]!="" || $_POST["P_phone_number"]!="" ||$_POST["P_mobile_number"]!="")
 function validatePersonalInfoOnSave($personalInfo)
 {
-	$k='';
+	//$count=0;
+	$message='';
+	//1
 	if($personalInfo['Full_Name']!='')
 	{
 		$fullName=$personalInfo['Full_Name'];
 		if(!hasOnlyAlphabets($fullName))
 		{
-			$k='Enter a Valid name. Only Alphabets<br/>';
+			$message='Enter a Valid name. Only Alphabets<br/>';
 		}
+		/*else
+		{
+			$count++;
+		}*/
 	}
-	if($personalInfo['gender']!='M'||$personalInfo['gender']!='F')
+	
+	if($personalInfo['gender']!='Male'||$personalInfo['gender']!='Female')
 	{
-		$k.='Enter Gender<br/.';
+		$message.='Enter Gender<br/.';
 	}
+	//2
 	if($personalInfo['date1']!='')
 	{
 		if(!validateDate($personalInfo['date1']))
 		{
-			$k.="Enter a Valid date<br/>";
+			$message.="Enter a Valid date<br/>";
 		}
+		/*else
+		{
+			$count++;
+		}*/
 	}
+	//3
 	if($personalInfo['fname']!="")
 	{
 		if(!hasOnlyAlphabets($personalInfo['fname']))
 		{
-			$k.="Enter a Valid Father's Name. Only Alphabets are Allowed<br/>";
+			$message.="Enter a Valid Father's Name. Only Alphabets are Allowed<br/>";
 		}
+		/*else
+		{
+			$count++;
+		}*/
 	}
+
 	if($personalInfo['pemail']!='')
 	{
 		if(!(filter_var($personalInfo['pemail'], FILTER_VALIDATE_EMAIL))
 		{
-			$k.="Enter a valid Primary Email Address.";
+			$message.="Enter a valid Primary Email Address.";
 		}
+		/*else
+		{
+			$count++;
+		}*/
 	}
 	
 	if($personalInfo['aemail']!='')
 	{
+		$check=true;
 		if(!(filter_var($personalInfo['aemail'], FILTER_VALIDATE_EMAIL))
 		{
-			$k.="Enter a valid Alternate Email Address.<br/>";
+			$message.="Enter a valid Alternate Email Address.<br/>";
+			$check=false;
 		}
 
 		if($personalInfo['pemail']=="")
 		{
-			$k.='Fill Primary Email first.<br/>';
-		}		
+			$message.='Fill Primary Email first.<br/>';
+			$check=false;
+		}
+
+		if($personalInfo['pemail']!=''&&$personalInfo['pemail']==$personalInfo['aemail'])
+		{
+			$message.="Primary and Alternate Email Can't be same. Fill in different one else leave it.<br/>";
+			$check=false;
+		}
+		if($check)
+		{
+			$count++;
+		}
 	}
 
-	if($k=='')
+	/*if($personalInfo['Temp_Address']!='')
+	{
+		$count++;
+	}*/
+
+	if($personalInfo['T_pincode']!='')
+	{
+		if(!hasOnlyCharacters('0-9-',$personalInfo['T_pincode']))
+		{
+			$message.="Please Enter  Valid Pincode. Numerics Only.<br/>";
+		}
+		/*else
+		{
+			$count++;
+		}*/
+	}
+
+	if($personalInfo['T_phone_number']!='')
+	{
+
+		if(!hasOnlyNumbers($personalInfo['T_phone_number']))
+		{
+			$message.="Please Enter Valid phone number. Enter Only Numbers."
+		}
+		/*else
+		{
+			$count++;
+		}*/
+	}
+
+	if($personalInfo['T_mobile_number']!='')
+	{
+		if(!hasOnlyNumbers($personalInfo['T_mobile_number']))
+		{
+			$message.="Please Enter Valid mobile number. Enter Only Numbers."
+		}
+		/*else
+		{
+			$count++;
+		}*/
+	}
+
+	/*if($personalInfo['perm_Address']!='')
+	{
+		$count++;
+	}*/
+
+	if($personalInfo['P_pincode']!='')
+	{
+		if(!hasOnlyCharacters('0-9-',$personalInfo['P_pincode']))
+		{
+			$message.="Enter Valid Pincode in Permanent Address.<br/>";
+		}
+		/*else
+		{
+			$count++;
+		}*/
+	}
+
+	if($personalInfo['P_phone_number']!='')
+	{
+		if(!hasOnlyNumbers($personalInfo['P_phone_number']))
+		{
+			$message.="Enter valid phone number in Permanent Address. Enter Only Numbers<br/>";
+		}
+		/*else
+		{
+			$count++;
+		}*/
+	}
+
+	if($personalInfo['P_mobile_number']!='')
+	{
+		if(!hasOnlyNumbers($personalInfo['P_mobile_number']))
+		{
+			$message.="Enter Valid mobile number in Permanent Address. Enter Only Numbers.<br/>";
+		}
+		/*else
+		{
+			$count++;
+		}*/
+	}
+
+	
+	if($message=='')
 	{
 		return true;
 	}
 	else
 	{
-		return $k;
+		return $message;
 	}
 	
 
